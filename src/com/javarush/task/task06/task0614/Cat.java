a024c56a9d490f0924f9c720ddc5825291c885f3
moshkina-nataliia <gubskaya.natasha@gmail.com>
2023-11-21 23:25:13 +0300

    Adding new tasks

---
+++ src/com/javarush/task/task06/task0614/Cat.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task06.task0614;
+
+import java.util.ArrayList;
+
+/*
+Статические коты
+*/
+
+public class Cat {
+    public static ArrayList<Cat> cats = new ArrayList<>();
+
+    public Cat() {
+    }
+
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+        Cat cat3 = new Cat();
+        Cat cat4 = new Cat();
+        Cat cat5 = new Cat();
+        Cat cat6 = new Cat();
+        Cat cat7 = new Cat();
+        Cat cat8 = new Cat();
+        Cat cat9 = new Cat();
+        Cat cat10 = new Cat();
+        cats.add(cat1);
+        cats.add(cat2);
+        cats.add(cat3);
+        cats.add(cat4);
+        cats.add(cat5);
+        cats.add(cat6);
+        cats.add(cat7);
+        cats.add(cat8);
+        cats.add(cat9);
+        cats.add(cat10);
+        printCats();
+    }
+
+    public static void printCats() {
+
+        for (int i = 0; i < cats.size(); i++) {
+            System.out.println(cats.get(i));
+        }
+    }
+}
