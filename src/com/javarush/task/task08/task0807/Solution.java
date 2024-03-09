7daef76152b18a4a85430b0734d7335ad2fd8e9d
moshkina-nataliia <gubskaya.natasha@gmail.com>
2024-03-09 17:47:51 +0300

    Adding new tasks

---
+++ src/com/javarush/task/task08/task0807/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task08.task0807;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/*
+LinkedList и ArrayList
+*/
+
+public class Solution {
+    public static Object createArrayList() {
+        ArrayList <Object> list = new ArrayList<>();
+return list;
+    }
+
+    public static Object createLinkedList() {
+        LinkedList<Object> objectLinkedList = new LinkedList<>();
+        return objectLinkedList;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
