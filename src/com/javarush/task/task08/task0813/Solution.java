7fe47a0551681eda0aed21b3343aef6baaf7f158
moshkina-nataliia <gubskaya.natasha@gmail.com>
2024-04-03 00:24:07 +0300

    Adding new tasks

---
+++ src/com/javarush/task/task08/task0813/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task08.task0813;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+20 слов на букву «Л»
+*/
+
+public class Solution {
+    public static Set<String> createSet() {
+        Set<String> createSet= new HashSet<String>(20);
+        createSet.add("любовь");
+        createSet.add("лень");
+        createSet.add("ласка");
+        createSet.add("лоно");
+        createSet.add("лес");
+        createSet.add("лис");
+        createSet.add("лак");
+        createSet.add("ларь");
+        createSet.add("лапа");
+        createSet.add("лапти");
+
+        createSet.add("лада");
+        createSet.add("лодка");
+        createSet.add("леска");
+        createSet.add("лист");
+        createSet.add("Людмила");
+        createSet.add("Лариса");
+        createSet.add("лохотрон");
+        createSet.add("литр");
+        createSet.add("литургия");
+        createSet.add("лань");
+
+return createSet;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
